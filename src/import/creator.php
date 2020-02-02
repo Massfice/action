b546d1cd7776fbcb948e1a916d1feb858d3b0470
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2020-02-02 10:58:07 +0100

    Added massfice.si.json and import file for Smart/Import

---
+++ src/import/creator.php
@@ -0,0 +1,9 @@
+<?php
+
+use Massfice\Action\HtmlAction;
+use Massfice\Action\JsonAction;
+use Massfice\Action\VerifyStatus;
+use Massfice\ResponseStatus\ResponseStatus;
+use Massfice\ResponseStatus\ResponseStatusFactory;
+
+?>
